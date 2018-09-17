d282856914bdc993b8105c19654435f4360e9d72
mtfum <f.yamanaka1127@gmail.com>
2018-09-17 19:56:53 +0700

    137

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -37,4 +37,19 @@ describe('Campaigns', () => {
     assert.ok(factory.options.address);
     assert.ok(factory.options.address);
   });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('alloes to contribute money annd marks then as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    });
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
 });
