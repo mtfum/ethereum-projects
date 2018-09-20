f9f2b131aa2707e5d7dd0623d28c8bd3c3b16904
mtfum <f.yamanaka1127@gmail.com>
2018-09-20 09:43:39 +0700

    152, fix typo

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web3'
 import CampaignFactory from './build/CampaignFactory'
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.intinterface),
+  JSON.parse(CampaignFactory.interface),
   '0xf4F06B587A9a22801B5039391fc503FC278F20dD'
 )
 
