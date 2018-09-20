65c4ceec9cddeeec29e3ae523eac1aeab3c74c46
mtfum <f.yamanaka1127@gmail.com>
2018-09-20 10:35:11 +0700

    156

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import factory from '../ethereum/factory'
-
+import { Card } from 'semantic-ui-react'
 class CampaignIndex extends Component {
 
   static async getInitialProps() {
@@ -9,9 +9,21 @@ class CampaignIndex extends Component {
     return { campaigns }
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    })
+
+    return <Card.Group items={items} />
+  }
+
   render() {
 
-    return <div>{ this.props.campaigns[0] }</div>
+    return <div>{ this.renderCampaigns() }</div>
   }
 }
 
