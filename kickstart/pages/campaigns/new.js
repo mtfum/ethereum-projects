88a02e87b51d6e62467d1a7c692f40ea4dd551f2
mtfum <f.yamanaka1127@gmail.com>
2018-09-22 19:18:35 +0700

    166

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
+import { Button, Form } from 'semantic-ui-react'
 
 class CampaignNew extends Component {
   render() {
 
     return (
       <Layout>
-        <h1>New Campaign!</h1>
+        <h3>Create a Campaign!</h3>
+
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <input />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </ Form>
       </Layout>
     )
   }
