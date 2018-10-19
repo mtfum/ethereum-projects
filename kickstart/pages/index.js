cc0032342a10623f848b7e56ccc6f19f078466e2
mtfum <f.yamanaka1127@gmail.com>
2018-10-19 08:08:23 +0700

    176

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import factory from '../ethereum/factory'
 import { Card, Button } from 'semantic-ui-react'
 import Layout from '../components/Layout'
-
+import { Link } from '../routes'
 
 class CampaignIndex extends Component {
 
@@ -16,7 +16,11 @@ class CampaignIndex extends Component {
     const items = this.props.campaigns.map(address => {
       return {
         header: address,
-        description: <a>View Campaign</a>,
+        description: (
+            <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+          ),
         fluid: true
       }
     })
@@ -31,11 +35,15 @@ class CampaignIndex extends Component {
         <div>
           <h3>Open Campaign</h3>
 
+          <Link route="campaigns/new">
+          <a>
           <Button floated="right"
             content= "Create Campaign"
             icon="add circle"
             primary
           />
+          </a>
+          </Link>
 
           { this.renderCampaigns() }
           </div>
