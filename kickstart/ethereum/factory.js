0ab505a3fb2232de19f887d85960b40195ec080d
mtfum <f.yamanaka1127@gmail.com>
2018-10-19 09:40:26 +0700

    181

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory'
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xf4F06B587A9a22801B5039391fc503FC278F20dD'
+  '0xe23a8aDEd3C29A92F7d1067025a4b61F5D079c32'
 )
 
 export default instance
