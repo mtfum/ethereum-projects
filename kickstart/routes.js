ecf1033731554c919638f4365702839309643696
mtfum <f.yamanaka1127@gmail.com>
2018-10-19 08:18:04 +0700

    177

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -1,6 +1,9 @@
 const routes = require('next-routes')()
 
-// routes.add()
+routes
+  .add('/campaigns/new', '/campaigns/new')
+  .add('/campaigns/:address', 'campaigns/show')
+
 
 module.exports = routes
 
