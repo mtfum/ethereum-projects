0cf6238dc91767067e6d4b51130ca420680132b4
mtfum <f.yamanaka1127@gmail.com>
2018-10-19 10:58:59 +0700

    192

--- kickstart/components/contributeForm.js
+++ kickstart/components/contributeForm.js
@@ -6,13 +6,17 @@ import { Router } from '../routes'
 
 class ContributeForm extends Component {
   state = {
-    value: ''
+    value: '',
+    errorMessage: '',
+    loadinng: false
   }
 
   onSubmit = async (event) => {
     event.preventDefault()
 
     const campaign = Campaign(this.props.address)
+
+    this.setState({ loading: true, errorMessage: '' })
 
     try {
       const accounts = await web3.eth.getAccounts()
@@ -23,14 +27,16 @@ class ContributeForm extends Component {
 
       Router.replaceRoute(`/campaigns/${this.props.address}`)
     } catch (err) {
-
+      this.setState({ errorMessage: err.message })
     }
+
+    this.setState({ loading: false, value: '' })
   }
 
   render() {
 
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -40,7 +46,8 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>
+        <Message error header="Oops!" content={ this.state.errorMessage } />
+        <Button primary loading={this.state.loadinng}>
           Contribute!
         </Button>
 
