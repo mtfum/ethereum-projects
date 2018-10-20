4faaf42cd639eb27b65a2a874c36fbdd361aec79
mtfum <f.yamanaka1127@gmail.com>
2018-10-20 07:23:21 +0700

    194

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -60,24 +60,28 @@ class CampaignShow extends Component {
   }
 
   render() {
-    return (
-      <Layout>
-        <h3>Campaign Show</h3>
-        <Grid>
-          <Grid.Column width={10}>
-            {this.renderCards()}
-            <Link route={`/campaigns/${this.props.address}/requests`}>
-              <a>
-                <Button primary>View Requests</Button>
-              </a>
-            </Link>
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <ContributeForm address={this.props.address} />
-          </Grid.Column>
-        </Grid>
-      </Layout>
-    )
+    return <Layout>
+				<h3>Campaign Show</h3>
+				<Grid>
+					<Grid.Row>
+						<Grid.Column width={10}>
+              {this.renderCards()}
+						</Grid.Column>
+						<Grid.Column width={6}>
+							<ContributeForm address={this.props.address} />
+						</Grid.Column>
+					</Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Link route={`/campaigns/${this.props.address}/requests`}>
+                <a>
+                  <Button primary>View Requests</Button>
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
+				</Grid>
+			</Layout>;
   }
 }
 
