6ca9e57b5c73a0631b81d1f7075f4afa7013275c
mtfum <f.yamanaka1127@gmail.com>
2018-10-20 10:03:29 +0700

    202

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -12,7 +12,7 @@ class RequestIndex extends Component {
     const requestCount = await campaign.methods.getRequestsCount().call()
 
     const requests = await Promise.all(
-      Array(requestCount)
+      Array(parseInt(requestCount))
         .fill()
         .map((element, index) => {
           return campaign.methods.requests(index).call()
